29a3018b8ffca71142f5e6436a26296179b8df33
Unknown <anelson@apocryph.org>
2017-04-03 08:37:46 -0400

    Don't enable HyperV since it interfers with VMWare Workstation

--- packages/enable_windows_features.ps1
+++ packages/enable_windows_features.ps1
@@ -7,8 +7,9 @@ if ($features | Where-Object {$_ -like "*Linux*"})
     choco install Microsoft-Windows-Subsystem-Linux           --source windowsfeatures --limitoutput
 }
 
-# windows containers
-Enable-WindowsOptionalFeature -Online -FeatureName containers -All
+# VMWare Workstation won't work if Hyper-V is enabled, so it's one or the other
+# # windows containers
+# Enable-WindowsOptionalFeature -Online -FeatureName containers -All
 
-# hyper-v (required for windows containers)
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+# # hyper-v (required for windows containers)
+# Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
